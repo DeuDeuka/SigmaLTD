dcb69625cfe498f5157f402d018e1573effd2d7e
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2025-04-16 22:45:35 +0700

    Restructured login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "@/app/screens/LoginScreen";
+
+type RootStackParamList = {
+  Login: undefined;
+  Profile: { userId: string };
+  Feed: { sort: 'latest' | 'top' } | undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 export default function RootLayout() {
-  return <Stack />;
+  return <RootStack.Navigator initialRouteName="Login">
+    <RootStack.Screen
+        name="Login"
+        component={LoginScreen} />
+  </RootStack.Navigator>;
 }
